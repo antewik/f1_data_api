6f2c5f6f291ad2269aadbbd9648beb0e9e04abad
Andreas Wikström <andreas.wikstrom@agio.se>
2025-10-04 11:33:45 -0700

    New endpoint - ping

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from ping import ping_bp
 from race_info import race_info_bp
 from team_standings import team_standings_bp
 from driver_standings import driver_standings_bp
@@ -8,6 +9,8 @@ from flasgger import Swagger
 app = Flask(__name__)
 CORS(app)
 
+
+app.register_blueprint(ping_bp)
 app.register_blueprint(race_info_bp)
 app.register_blueprint(team_standings_bp)
 app.register_blueprint(driver_standings_bp)
