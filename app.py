061c0dd003a06b595838823272b8aa8b8f521b3e
Andreas Wikström <andreas.wikstrom@agio.se>
2025-11-11 15:52:47 -0800

    Upgraded to Python 3.11.9

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ swagger_config = {
         {
             "endpoint": 'apispec_1',
             "route": '/apispec_1.json',
-            "rule_filter": lambda rule: True,  # include all endpoints
-            "model_filter": lambda tag: True,  # include all models
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
         }
     ],
     "static_url_path": "/flasgger_static",
